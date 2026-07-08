1f51c071336577e270d6c65b6aa43140bd6cac50
Sarah Rivera <sarahannrivera25@gmail.com>
2026-07-07 20:09:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav.jsx'
+import Footer from './components/Footer.jsx';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/pages/Home.jsx';
+import Books from './components/pages/Books.jsx';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Nav />
+     <Route path="/"  exact component={Home} />
+     <Route path="/books" component={Books} />
+     <Footer />
+
     </div>
+    </Router>
   );
 }
 
