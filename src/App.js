9f50cb5e04db93ac7aacbcb2c997712a3ca7d45b
Sarah Rivera <sarahannrivera25@gmail.com>
2026-07-13 15:43:36 -0400

    Almost

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import Nav from './components/Nav.jsx'
+import React, { useState, useEffect } from 'react';
 import Footer from './components/Footer.jsx';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/pages/Home.jsx';
 import Books from './components/pages/Books.jsx';
 import { books } from './data'
 import BookInfo from './components/pages/BookInfo.jsx';
+import Cart from './components/pages/Cart.jsx';
 
 
 function App() {
+  const [Cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...Cart, {...book, quantity: 1}])
+   
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(Cart.map(item => 
+       item.id === book.id 
+      ?
+        {
+          ...item,
+          quantity: +quantity,
+        }
+      :
+    
+        item
+      
+
+    ));
+  }
+
+  function removeItem(item) {
+    setCart(Cart.filter(book => book.id !== item.id))
+    console.log('removeItem', item)
+  }
+
+ function numberOfItems() {
+  let counter = 0;
+  Cart.forEach(item => {
+    counter += item.quantity
+  })
+  return counter;
+ }
+
+  useEffect(() => {
+    console.log(Cart);
+  }, [Cart])
+
   return (
     <Router>
     <div className="App">
-     <Nav />
+     <Nav numberOfItems={numberOfItems()}/>
      <Switch>
      <Route path="/"  exact component={Home} />
      <Route path="/books" exact render={() => <Books books={books} />} />
-     <Route path="/books/:id" exact render={() => <BookInfo books={books} />} />
+     <Route path="/books/:id" exact render={() => <BookInfo books={books} addToCart={addToCart}/>} />
+     <Route path="/Cart" render={() => <Cart books={books} Cart={Cart} changeQuantity= {changeQuantity} removeItem={removeItem}/>} />
      </Switch>
      <Footer />
 
--- src/components/pages/BookInfo.jsx
+++ src/components/pages/BookInfo.jsx
@@ -1,15 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Rating from '../Rating';
 import Price from '../ui/Price';
 import { useParams } from 'react-router-dom';
 import Book from '../ui/Book';
+import Cart from "./Cart";
 
-const BookInfo = ( {books} ) => {
+const BookInfo = ( {books, addToCart, Cart} ) => {
      const {id} = useParams();
      const book = books.find(book => +book.id === +id);
-     console.log(book)
+     const [added, setAdded] = useState=(false);
+
+     function addBookToCart(book) {
+        addToCart(book);
+     }
+
+     if (!book) {
+  return <div>Book not found</div>;
+}
+     function bookExistsOnCart () {
+        return Cart.find(book => book.id === +id);
+
+     }
+
     return (
         <div id="books__body">
             <main id="books__main">
@@ -45,7 +59,13 @@ const BookInfo = ( {books} ) => {
                                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos ex cum impedit, placeat eveniet nobis harum necessitatibus doloremque labore fuga porro voluptates a perferendis itaque officiis voluptatum! Alias, voluptatibus labore?
                                     </p>
                                 </div>
-                                <button className="btn">Add to Cart</button>
+                                
+                                    bookExistsOnCart() ? (
+                                        <Link to ={`/cart/${book.id}`} classNamw="book__link">
+                                        <button className="btn">Checkout</button> :   <button className="btn" onCLick={() => addBookToCart(book)} >Add to Cart</button>
+                                       </Link>)
+                                
+                              
                             </div>
                         </div>
                     </div>
